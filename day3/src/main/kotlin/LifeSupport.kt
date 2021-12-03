5076cd436410bc5b06175a4de77d2804fa8d0ea2
Nguyen Van, Sven <s.nguyen@optadata-gruppe.de>
2021-12-03 17:26:43 +0100

    ✨ Started 3.2

---
+++ day3/src/main/kotlin/LifeSupport.kt
@@ -0,0 +1,9 @@
+fun lifeSupportRating(diagnosticReport : List<List<Int>>) :Int = oxygenRating(diagnosticReport) * co2ScrubberRating(diagnosticReport)
+
+fun co2ScrubberRating(diagnosticReport: List<List<Int>>): Int {
+    TODO("Not yet implemented")
+}
+
+fun oxygenRating(diagnosticReport: List<List<Int>>): Int {
+    TODO("Not yet implemented")
+}
---
+++ day3/src/main/kotlin/PowerConsumption.kt
@@ -0,0 +1,32 @@
+import kotlin.math.roundToInt
+
+fun powerConsumption(data: List<List<Int>>) = epsilon(data) * gamma(data)
+fun epsilon(data: List<List<Int>>): Int = binaryEpsilon(data)
+    .toInt(2)
+
+fun binaryEpsilon(data: List<List<Int>>): String  = averageColumns(data)
+    .map(Double::roundToInt)
+    .map(::flipBit)
+    .joinToString(separator = "")
+
+private fun flipBit(it: Int) = (it - 1) * -1
+fun gamma(data: List<List<Int>>) = binaryGamma(data)
+    .toInt(2)
+
+fun binaryGamma(data: List<List<Int>>) = averageColumns(data)
+    .map(Double::roundToInt)
+    .joinToString(separator = "")
+
+private fun averageColumns(
+    data: List<List<Int>>
+) = addColumns(data)
+        .map(Int::toDouble)
+        .map { it / data.size }
+
+private fun addColumns(data: List<List<Int>>) = data.reduce { total, line ->
+    total
+        .zip(line)
+        .map { (avg, i) ->
+            avg + i
+        }
+}
