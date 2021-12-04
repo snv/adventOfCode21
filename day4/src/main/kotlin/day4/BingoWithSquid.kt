2955dd1e8fbd27b309e2c30f3a6d1d11fd5f28f2
Nguyen Van, Sven <s.nguyen@optadata-gruppe.de>
2021-12-04 19:27:16 +0100

    day 4 done

--- day4/src/main/kotlin/day4/BingoWithSquid.kt
+++ day4/src/main/kotlin/day4/BingoWithSquid.kt
@@ -3,8 +3,26 @@ package day4
 fun main() {
     println("""
         Part 1:
-            
+            ${pickWinningBoard().finalScore}
         Part 2:
-            
+            ${pickLoosingBoard().finalScore}
     """.trimIndent())
 }
+
+fun pickWinningBoard() = rawInput
+    .parseInput()
+    .let { (drawnNumbers, boards) ->
+        boards
+            .map { PlayedBingoBoard(it, drawnNumbers) }
+            .sortedBy { it.winsAfter }
+            .first()
+    }
+
+fun pickLoosingBoard() = rawInput
+    .parseInput()
+    .let { (drawnNumbers, boards) ->
+        boards
+            .map { PlayedBingoBoard(it, drawnNumbers) }
+            .sortedByDescending { it.winsAfter }
+            .first()
+    }
