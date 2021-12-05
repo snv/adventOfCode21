a0ca1583cfb6dab2cdec786f28bd4fe818e2f896
Nguyen Van, Sven <s.nguyen@optadata-gruppe.de>
2021-12-05 16:34:15 +0100

    🚧 generating a heatmap

--- day5/src/main/kotlin/day5/ThermalMap.kt
+++ day5/src/main/kotlin/day5/ThermalMap.kt
@@ -1,12 +1,43 @@
 package day5
 
+import jdk.jfr.Threshold
+
 typealias Coordinate = Pair<Int,Int>
 typealias Vents = Pair<Coordinate, Coordinate>
+typealias HeatMap = Map<Coordinate, Int>
 
+@JvmName("plusCoords")
+operator fun HeatMap.plus(coordinate: Coordinate) : HeatMap = plus(
+    coordinate to getOrDefault(coordinate, 0) + 1
+)
 
-@JvmInline
-value class ThermalMap(val lines : Collection<Vents>){
-    init {
-
-    }
+fun Vents.expand() : List<Coordinate> = let { (start, end) ->
+    (if (start.first <= end.first) (start.first..end.first) else (end.first..start.first))
+        .flatMap { first ->
+            (if (start.second <= end.second) (start.second..end.second) else (end.second..start.second))
+                .map { second -> Coordinate(first,second) }
+        }
 }
+
+
+@JvmName("plusVents")
+operator fun HeatMap.plus(vents: Vents) :HeatMap = vents.expand()
+    .fold(this) { heatmap, vent ->  heatmap + vent }
+
+fun buildHeatMap(lines : Collection<Vents>): HeatMap = lines
+    .fold(emptyMap()) {
+        heatMap: HeatMap, vents ->
+        heatMap + vents
+    }
+
+@Suppress("unused") //for debugging
+fun HeatMap.draw() =
+    (0..9).joinToString(separator = "", prefix = "${this}\n") { second ->
+        (0..9)
+            .map { this[it to second] }
+            .map { it?.digitToChar() ?: '.' }
+            .joinToString(separator = "", postfix = "\n", prefix = "\t")
+    }
+
+fun HeatMap.findHotspots(threshold: Int) =
+    filter { (_, value) -> value >= threshold }
---
+++ day5/src/test/kotlin/day5/ThermalMapKtTest.kt
@@ -0,0 +1,129 @@
+@file:Suppress("ClassName")
+
+package day5
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+
+internal class ThermalMapKtTest {
+    enum class ExpandedSampleVents(val coordinates: List<Coordinate>){
+        LONG_HORIZONTAL(listOf(
+            0 to 9,
+            1 to 9,
+            2 to 9,
+            3 to 9,
+            4 to 9,
+            5 to 9
+        )),
+        SHORT_HORIZONTAL(listOf(
+            0 to 9,
+            1 to 9,
+            2 to 9
+        )),
+        VERTICAL(listOf(
+            7 to 0,
+            7 to 1,
+            7 to 2,
+            7 to 3,
+            7 to 4
+        ))
+    }
+    enum class SampleVents(val vents: Vents){
+        LONG_HORIZONTAL((0 to 9) to (5 to 9)),
+        SHORT_HORIZONTAL((0 to 9) to (2 to 9)),
+        VERTICAL((7 to 0) to (7 to 4))
+    }
+
+    val sampleInput = listOf(
+        ((0 to 9) to (5 to 9)),
+        ((8 to 0) to (0 to 8)),
+        ((9 to 4) to (3 to 4)),
+        ((2 to 2) to (2 to 1)),
+        ((7 to 0) to (7 to 4)),
+        ((6 to 4) to (2 to 0)),
+        ((0 to 9) to (2 to 9)),
+        ((3 to 4) to (1 to 4)),
+        ((0 to 0) to (8 to 8)),
+        ((5 to 5) to (8 to 2))
+    )
+
+    @Test
+    fun `add coordinates to heatmap`() {
+        val heatmap = emptyMap<Coordinate, Int>() + (0 to 9) + (7 to 0) + (0 to 9)
+
+        assertEquals(
+            mapOf(
+                (7 to 0) to 1,
+                (0 to 9) to 2
+            ),
+            heatmap
+        )
+    }
+
+    @Nested
+    inner class `expand vents` {
+        @Test
+        fun horzontaly() {
+            //act
+            val expandedVents = SampleVents.LONG_HORIZONTAL.vents.expand()
+
+            //assert
+            assertEquals(
+                ExpandedSampleVents.LONG_HORIZONTAL.coordinates,
+                expandedVents
+            )
+        }
+
+        @Test
+        fun vertically() {
+            //arrange
+            val sampleVents : Vents = SampleVents.VERTICAL.vents
+
+            //act
+            val expandedVents = sampleVents.expand()
+
+            //assert
+            assertEquals(
+                ExpandedSampleVents.VERTICAL.coordinates,
+                expandedVents
+            )
+        }
+    }
+
+    @Test
+    fun `add lines of vents to heatmap`() {
+        val heatmap = emptyMap<Coordinate, Int>() +
+            SampleVents.LONG_HORIZONTAL.vents +
+            SampleVents.SHORT_HORIZONTAL.vents +
+            SampleVents.VERTICAL.vents
+
+        //println(heatmap.draw())
+
+        assertNull(heatmap[9 to 9])
+        assertEquals(2, heatmap[0 to 9])
+        assertEquals(2, heatmap[2 to 9])
+        assertEquals(1, heatmap[3 to 9])
+        assertEquals(1, heatmap[7 to 2])
+        assertEquals(1, heatmap[7 to 4])
+    }
+
+    @Test
+    fun buildHeatMap() {
+        val filtered = sampleInput
+            .filter { (start, end) -> start.first == end.first || start.second == end.second }
+            .also(::println)
+
+        println(buildHeatMap(filtered).draw())
+    }
+
+    @Test
+    fun findHotspots(){
+        val filtered = sampleInput
+            .filter { (start, end) -> start.first == end.first || start.second == end.second }
+
+        val hotspots = buildHeatMap(filtered).findHotspots(2)
+
+        assertEquals(5, hotspots.size)
+    }
+}
