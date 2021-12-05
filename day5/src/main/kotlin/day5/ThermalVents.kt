68c7783a6b11f3ac7f75bc6e941499daca996746
Nguyen Van, Sven <s.nguyen@optadata-gruppe.de>
2021-12-05 18:07:05 +0100

    day 5 done

--- day5/src/main/kotlin/day5/ThermalVents.kt
+++ day5/src/main/kotlin/day5/ThermalVents.kt
@@ -1,15 +1,22 @@
 package day5
 
 fun main() {
+    val input = rawInput.parse()
     println("""
         Part 1:
-            ${countHotspotsForPart1()}
+            ${countHotspotsForPart1(input)}
         Part 2:
+            ${countHotspotsForPart2(input)}
     """.trimIndent())
 }
 
-fun countHotspotsForPart1() = rawInput.parse()
+fun countHotspotsForPart1(input: List<Vents>) = input
     .filter { (start, end) -> start.first == end.first || start.second == end.second }
+    .let(::buildHeatMap)
+    .findHotspots(2)
+    .count()
+
+fun countHotspotsForPart2(input: List<Vents>) = input
     .let(::buildHeatMap)
     .findHotspots(2)
     .count()
